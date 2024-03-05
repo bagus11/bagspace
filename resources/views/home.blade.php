cefe993ca00119d43922fc6860615b731a6e872d
unknown <bagus.oetomo11@gmail.com>
2024-03-05 16:08:07 +0700

    templating is done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,24 @@
-@extends('layouts.app')
-
+@extends('layouts.admin')
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                <div class="card-header pb-0">
+                  <p>Dashboard</p>
                 </div>
-            </div>
+                <div class="card-body">
+                  <p class="text-uppercase text-sm">About me</p>
+                  <div class="row">
+                    <div class="col-md-12">
+                      <div class="form-group">
+                        <label for="example-text-input" class="form-control-label">About me</label>
+                        <input class="form-control" type="text" value="A beautiful Dashboard for Bootstrap 5. It is Free and Open Source.">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
         </div>
     </div>
 </div>
