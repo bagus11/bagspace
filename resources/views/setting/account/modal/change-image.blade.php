9e1ed2836a3d4e0c9f3c35fc9158aeddb4bf4193
unknown <bagus.oetomo11@gmail.com>
2024-07-31 11:25:08 +0700

    update image

--- resources/views/setting/account/modal/change-image.blade.php
+++ resources/views/setting/account/modal/change-image.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="changeImageModal">
-    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-dialog modal-md modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header p-0 my-2 mx-2">
                 <b style="font-size:13px;margin-top:5px;margin-left:5px">Change Image</b>
@@ -17,16 +17,17 @@
                     </div>
                 </div>
                 <div class="row mt-3">
-                    <div class="col-12" id="cropContainer" style="display:none;">
-                        <img id="cropImage" style="max-width: 100%;">
-                        <button id="cropButton" class="btn btn-primary mt-2">Crop</button>
+                    <div class="container">
+                        <div class="col-12" id="cropContainer" style="display:none;">
+                            <img id="cropImage" style="max-width: 100%;">
+                            <button id="cropButton" class="btn btn-primary mt-2">
+
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
             <div class="modal-footer justify-content-end">
-                <button id="btn_update_profile" type="button" class="btn btn-success btn-sm">
-                    <i class="fas fa-check"></i>
-                </button>
             </div>
         </div>
     </div>
