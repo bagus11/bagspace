ef673bbda5a6de429415ad468cbfa7ad190e62ae
unknown <bagus.oetomo11@gmail.com>
2025-02-26 19:49:03 +0700

    update booking room device

--- app/Models/Booking/MeetingLink.php
+++ app/Models/Booking/MeetingLink.php
@@ -10,4 +10,9 @@ class MeetingLink extends Model
     use HasFactory;
     protected $table = 'meeting_room_model';
     protected $guarded = [];
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
 }
